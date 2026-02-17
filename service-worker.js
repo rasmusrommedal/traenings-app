563a15ae5b0006fcfe43e295fbfc27250993b51a
rasmusrommedal <rasmusjp@gmail.com>
2026-02-17 14:32:42 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,42 @@
-self.addEventListener('install', function(e) {
+const CACHE_NAME = 'traenings-app-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './pokemonkort.jpg',
+  './manifest.json'
+];
+
+// Install event
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('app-cache').then(function(cache){
-      return cache.addAll([
-        './',
-        './index.html',
-        './pokemonkort.jpg'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
     })
   );
 });
